4879152f6b10f241d26cefabe20c2cc201810f1c
Brett Stedford <brett.stedford@ao.com>
2022-12-01 21:12:16 +0000

    d1-2 solution

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -1,31 +1,47 @@
-﻿
-var fileLines = File.ReadAllLines("input/day1-1.txt");
+﻿var fileLines = File.ReadAllLines("input/day1-1.txt");
 
-int highestCalories = 0;
-List<int> currentElfCalories = new List<int>();
-bool checkNextElf = false;
+var highestThreeCalories = new int[3];
+var currentElfCalories = new List<int>();
+var checkNextElf = false;
 
 foreach (var line in fileLines)
 {
     if (checkNextElf)
     {
         var elfCalories = currentElfCalories.Sum();
-
-        if (elfCalories > highestCalories)
-            highestCalories = elfCalories;
-
         currentElfCalories = new List<int>();
-        
         checkNextElf = false;
+
+        var length = highestThreeCalories.Length;
+
+        if (length < 3)
+        {
+            highestThreeCalories[length - 1] = elfCalories;
+            continue;
+        }
+
+        var lowest = highestThreeCalories.Min();
+
+        if (elfCalories > lowest)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                if (highestThreeCalories[i] == lowest)
+                {
+                    highestThreeCalories[i] = elfCalories;
+                    break;
+                }
+            }
+        }
     }
-    
+
     if (string.IsNullOrEmpty(line))
     {
         checkNextElf = true;
         continue;
     }
-    
+
     currentElfCalories.Add(int.Parse(line));
 }
 
-Console.WriteLine($"The highest calculated calories was: {highestCalories}");
+Console.WriteLine($"The sum of the highest three calculated calories was: {highestThreeCalories.Sum()}");
