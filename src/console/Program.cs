2cf03f2d572bc63dcf4041aa10df9e10fc030aa5
Brett Stedford <brett.stedford@ao.com>
2022-12-01 20:55:19 +0000

    Initial commit with d1-1 solution

---
+++ src/console/Program.cs
@@ -0,0 +1,31 @@
+﻿
+var fileLines = File.ReadAllLines("input/day1-1.txt");
+
+int highestCalories = 0;
+List<int> currentElfCalories = new List<int>();
+bool checkNextElf = false;
+
+foreach (var line in fileLines)
+{
+    if (checkNextElf)
+    {
+        var elfCalories = currentElfCalories.Sum();
+
+        if (elfCalories > highestCalories)
+            highestCalories = elfCalories;
+
+        currentElfCalories = new List<int>();
+        
+        checkNextElf = false;
+    }
+    
+    if (string.IsNullOrEmpty(line))
+    {
+        checkNextElf = true;
+        continue;
+    }
+    
+    currentElfCalories.Add(int.Parse(line));
+}
+
+Console.WriteLine($"The highest calculated calories was: {highestCalories}");
