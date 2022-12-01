94ef77cc1204251fce02ddc872d2406b08a5739e
Brett Stedford <brett.stedford@ao.com>
2022-12-01 21:17:00 +0000

    chore: Some refactoring

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -1,47 +1,5 @@
-﻿var fileLines = File.ReadAllLines("input/day1-1.txt");
+﻿using console;
 
-var highestThreeCalories = new int[3];
-var currentElfCalories = new List<int>();
-var checkNextElf = false;
+var fileLines = File.ReadAllLines("input/day1-1.txt");
 
-foreach (var line in fileLines)
-{
-    if (checkNextElf)
-    {
-        var elfCalories = currentElfCalories.Sum();
-        currentElfCalories = new List<int>();
-        checkNextElf = false;
-
-        var length = highestThreeCalories.Length;
-
-        if (length < 3)
-        {
-            highestThreeCalories[length - 1] = elfCalories;
-            continue;
-        }
-
-        var lowest = highestThreeCalories.Min();
-
-        if (elfCalories > lowest)
-        {
-            for (var i = 0; i < 3; i++)
-            {
-                if (highestThreeCalories[i] == lowest)
-                {
-                    highestThreeCalories[i] = elfCalories;
-                    break;
-                }
-            }
-        }
-    }
-
-    if (string.IsNullOrEmpty(line))
-    {
-        checkNextElf = true;
-        continue;
-    }
-
-    currentElfCalories.Add(int.Parse(line));
-}
-
-Console.WriteLine($"The sum of the highest three calculated calories was: {highestThreeCalories.Sum()}");
+Console.WriteLine($"The sum of the highest three calculated calories was: {Solutions.Day1(fileLines)}");
---
+++ src/console/Solutions.cs
@@ -0,0 +1,53 @@
+﻿namespace console;
+
+public static class Solutions
+{
+    public static int Day1(string[] input)
+    {
+        var highestThreeCalories = new int[3];
+        var currentElfCalories = new List<int>();
+        var checkNextElf = false;
+
+        foreach (var line in input)
+        {
+            if (checkNextElf)
+            {
+                var elfCalories = currentElfCalories.Sum();
+                currentElfCalories = new List<int>();
+                checkNextElf = false;
+
+                var length = highestThreeCalories.Length;
+
+                if (length < 3)
+                {
+                    highestThreeCalories[length - 1] = elfCalories;
+                    continue;
+                }
+
+                var lowest = highestThreeCalories.Min();
+
+                if (elfCalories > lowest)
+                {
+                    for (var i = 0; i < 3; i++)
+                    {
+                        if (highestThreeCalories[i] == lowest)
+                        {
+                            highestThreeCalories[i] = elfCalories;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(line))
+            {
+                checkNextElf = true;
+                continue;
+            }
+
+            currentElfCalories.Add(int.Parse(line));
+        }
+
+        return highestThreeCalories.Sum();
+    }
+}
